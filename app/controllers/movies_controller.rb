3c94c33f6365479f0716fa3d26093c5186ca3423
spnutz <nutty1997@live.com>
2018-10-29 18:04:02 +0000

    Myrottenpotatoes can add movie and edit movie

---
+++ app/controllers/movies_controller.rb
@@ -0,0 +1,42 @@
+class MoviesController < ApplicationController
+    def index
+        @movies = Movie.all
+    end
+    
+    def show
+        id = params[:id]
+        @movie = Movie.find(id)
+    end
+    
+    def new
+        @movie = Movie.new
+    end
+    
+    def create
+        @movie = Movie.create!(allowparams)
+        flash[:notice] = "#{@movie.title} was successfully created."
+        redirect_to movie_path (@movie)
+    end
+    
+    def allowparams
+        params[:movie].permit(:title,:rating,:release_date)
+    end
+    
+    def edit
+        @movie = Movie.find params[:id]
+    end
+    
+    def update
+        @movie = Movie.find params[:id]
+        @movie.update_attributes!(allowparams)
+        flash[:notice] = "#{@movie.title} was successfully update"
+        redirect_to movie_path(@movie)
+    end
+    
+    def destroy
+        @movie = Movie.find params[:id]
+        @movie.destroy!(allowparams)
+        flash[:notice] = "Movie '#{@movie.title}' delete"
+        redirect_to movie_path
+    end
+end
---
+++ db/seeds.rb
@@ -0,0 +1,21 @@
+# This file should contain all the record creation needed to seed the database with its default values.
+# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
+#
+# Examples:
+#
+#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
+#   Character.create(name: 'Luke', movie: movies.first)
+more_movies = [
+    {:title => 'Aladin', :rating => 'G',
+        :release_date => '25-Nov-1992'},
+    {:title => 'When Harry Met Sally', rating => 'R',
+        :release_date => '21-Jul-1989'},
+    {:title => 'The Help', :rating => 'PG-13',
+        :release_date => '10-Aug-2011'},
+    {:title => 'Raiders of the Lost Ark', :rating => 'PG',
+        :release_date => '12-Jun-1981'}
+    ]
+
+more_movies.each do |movie|
+    movie.create!(movie)
+end
